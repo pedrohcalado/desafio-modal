c6dff9534009233c2856dd81e574306a8d5bb75f
Marcos Felipe Rocha <marcos.rocha@vaivoa.com>
2021-12-06 11:33:02 -0300

    :tada: Inicio Desafio Modal + SwiftLint
    Cria o projeto vazio no xcode 12.4 e adiciona o Swif

---
+++ DesafioModal/DesafioModal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesafioModal
+//
+//  Created by MARCOS FELIPE SOARES ROCHA on 06/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
