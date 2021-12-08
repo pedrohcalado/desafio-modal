ef937ec3da189472fe8c920369580bff47241cc8
Marco Antônio Marcelino <marco.marcelino@vaivoa.com>
2021-12-08 17:20:24 -0300

    feat: adiciona Xib com tela dos filtros

---
+++ DesafioModal/View/FilterViewController.swift
@@ -0,0 +1,30 @@
+// FilterViewController.swift
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func applyFilterTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearFiltersTapped(_ sender: UIButton) {
+    }
+}
